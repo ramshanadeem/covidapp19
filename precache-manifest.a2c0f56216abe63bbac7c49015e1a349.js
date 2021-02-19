91bf41a71377046f3467bc79e6abd800f66091c0
ramshanadeem <rimshanadeem64@gmail.com>
2021-02-19 10:47:21 +0500

    Updates

---
+++ precache-manifest.a2c0f56216abe63bbac7c49015e1a349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a510be009c456ba03f9928b1133b0ae6",
+    "url": "/covidapp19/index.html"
+  },
+  {
+    "revision": "1bdddda210dab161a6d6",
+    "url": "/covidapp19/static/css/main.68389b93.chunk.css"
+  },
+  {
+    "revision": "52003fe5fa1817149509",
+    "url": "/covidapp19/static/js/2.a8da93b8.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covidapp19/static/js/2.a8da93b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bdddda210dab161a6d6",
+    "url": "/covidapp19/static/js/main.24d846ff.chunk.js"
+  },
+  {
+    "revision": "485aafc285881f2c1db3",
+    "url": "/covidapp19/static/js/runtime-main.932b2420.js"
+  }
+]);
